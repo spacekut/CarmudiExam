8ba310a5dddde3c459353e97866cf872f68e6930
spacekut <katcabison@ymail.com>
2017-03-28 23:38:38 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup (
+	name="Carmudi",
+	version="1.0",
+	description="Exam",
+	author="Kat Cabison",
+	author_email="katcabison@ymail.com",
+	install_requires=[
+		'robotframework',
+		'robotframework-selenium2library',
+		'chromedriver_installer'
+	]
+)
